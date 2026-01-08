d9b701bbddd505536f6ee3d7a97b59518eee661e
lynnsengraving <lynnsengraving24@gmail.com>
2026-01-08 12:31:06 -0600

    Delete test_notifications.sh

--- test_notifications.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -X POST http://localhost:5001/api/notify   -H "Content-Type: application/json"   -d '{"type":"OrderUpdate","orderId":1,"status":"completed","contact":{"phone":"+16055551234","email":"john@demo.com"}}'
