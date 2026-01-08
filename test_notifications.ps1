914d64be9a7cd453c825ef617ec50145513fc16b
lynnsengraving <lynnsengraving24@gmail.com>
2026-01-08 12:31:25 -0600

    Delete test_notifications.ps1

--- test_notifications.ps1
+++
@@ -1,3 +0,0 @@
-curl -X POST http://localhost:5001/api/notify `
-  -H "Content-Type: application/json" `
-  -d '{"type":"OrderUpdate","orderId":1,"status":"completed","contact":{"phone":"+16055551234","email":"john@demo.com"}}'
